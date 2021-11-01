ff92ae9e8213ea2bbf1f333bec2f06df093b836f
Alexander Dolgan <5704737@gmail.com>
2021-11-01 23:23:31 +0300

    2021
    2021

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -78,9 +78,50 @@ function galleryTwo() {
 	element.classList.add('main-content-gallery-alt');
 }
 
+
+
+
+function setCookie( c_name, value, exdays ) {
+	var c_value = escape(value);
+	if (exdays) {
+		var exdate = new Date();
+		exdate.setDate( exdate.getDate() + exdays );
+		c_value += '; expires=' + exdate.toUTCString();
+	}
+	document.cookie = c_name + '=' + c_value;
+}
+
+function getCookie( c_name ) {
+	var i, x, y, cookies = document.cookie.split( ';' );
+
+	for ( i = 0; i < cookies.length; i++ ) {
+		x = cookies[i].substr( 0, cookies[i].indexOf( '=') );
+		y = cookies[i].substr( cookies[i].indexOf( '=') + 1 );
+		x = x.replace( /^\s+|\s+$/g, '' );
+
+		if ( x === c_name ) {
+			return unescape( y );
+		}
+	}
+}
+
+window.setTimeout(function(){ 
+	// When the cookie exists, do not show the popup!
+	if (getCookie('displayedPopupNewsletter')) {
+		return;
+	}
+
+	document.body.insertAdjacentHTML("afterbegin", `<div class="loader"><div class="preloader"></div></div>`);
+
+	// The popup was displayed. Set the cookie for 1 day.
+	setCookie('displayedPopupNewsletter', 'yes', 1);
+}, 3000);
+
+
 $(document).ready(function(){
+
 	let $loader = $('.loader');
-	$loader.delay( 3000 ).fadeOut( 'slow' );
+	$loader.delay( 2000 ).fadeOut( 'slow' );
 
   var allPanels = $('.description').hide();
     
@@ -134,55 +175,55 @@ $(document).ready(function(){
 		
   // })
 
-// start page parallax on mouse momve
-(function() {
+// // start page parallax on mouse momve
+// (function() {
 
-	function animate({timing, draw, duration}) {
+// 	function animate({timing, draw, duration}) {
 
-		let start = performance.now();
+// 		let start = performance.now();
 
-		requestAnimationFrame(function animate(time) {
-			// timeFraction изменяется от 0 до 1
-			let timeFraction = (time - start) / duration;
-			if (timeFraction > 1) timeFraction = 1;
+// 		requestAnimationFrame(function animate(time) {
+// 			// timeFraction изменяется от 0 до 1
+// 			let timeFraction = (time - start) / duration;
+// 			if (timeFraction > 1) timeFraction = 1;
 
-			// вычисление текущего состояния анимации
-			let progress = timing(timeFraction);
+// 			// вычисление текущего состояния анимации
+// 			let progress = timing(timeFraction);
 
-			draw(progress); // отрисовать её
+// 			draw(progress); // отрисовать её
 
-			if (timeFraction < 1) {
-				requestAnimationFrame(animate);
-			}
+// 			if (timeFraction < 1) {
+// 				requestAnimationFrame(animate);
+// 			}
 
-		});
-	}
-
-
-    document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector(".start-page-bg");
+// 		});
+// 	}
 
 
-		function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-
-			animate({
-				duration: 50,
-				timing: linearFunction => linearFunction,
-				draw(progress){
-					let x = (50 - (_mouseX - _w) * -0.01) * (progress/100);
-					let y = (50 - (_mouseX - _w) * -0.01) * (progress/100);
-					let _depth1 = `${50 - (_mouseX - _w) * -0.01}% ${50 - (_mouseY - _h) * -0.01}%`;
-					elem.style.backgroundPosition = _depth1;
-				}
-			});
+//     document.addEventListener("mousemove", parallax);
+//     const elem = document.querySelector(".start-page-bg");
 
 
-    }
-})();
+// 		function parallax(e) {
+//         let _w = window.innerWidth/2;
+//         let _h = window.innerHeight/2;
+//         let _mouseX = e.clientX;
+//         let _mouseY = e.clientY;
+
+// 			animate({
+// 				duration: 50,
+// 				timing: linearFunction => linearFunction,
+// 				draw(progress){
+// 					let x = (50 - (_mouseX - _w) * -0.01) * (progress/100);
+// 					let y = (50 - (_mouseX - _w) * -0.01) * (progress/100);
+// 					let _depth1 = `${50 - (_mouseX - _w) * -0.01}% ${50 - (_mouseY - _h) * -0.01}%`;
+// 					elem.style.backgroundPosition = _depth1;
+// 				}
+// 			});
+
+
+//     }
+// })();
 
 	$(".room-preview").lightGallery(
 		{
@@ -209,6 +250,7 @@ $(document).ready(function(){
 
 	// front page text slider
 	$( '.text-carousel' ).slick({
+		lazyLoad: 'ondemand',
 		dots: false,
 		arrows: false,
 		infinite: true,
@@ -218,7 +260,7 @@ $(document).ready(function(){
 		autoplaySpeed: 4000,
 		pauseOnHover: false,
 	});
-
+$('.text-carousel').show();
 
 
 	$('.room-preview').on('init', function(event, slick){
@@ -319,7 +361,7 @@ $(document).ready(function(){
 	if ( ! $slick_slider.hasClass('slick-initialized')) {
 		return $slick_slider.slick(settings);
 	}
-		
+
 });
 
 
